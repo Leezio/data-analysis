f236e6328d8a257262790e82da73b3c61f0a4b14
François Flament <contact@leezio.fr>
2018-10-04 11:06:08 +0200

    Adding fileinfo.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import argparse
+import fileinfo
 
 parser = argparse.ArgumentParser();
-parser.add_argument("-f","--file", nargs="+", help="Chemin du fichier de données");
+parser.add_argument("-f","--file", nargs="?", help="Chemin du fichier de donnees");
 parser.add_argument("-u","--user", action="store_true", help="Activer l'interaction utilisateur");
 parser.add_argument("-l","--log", action="store_true", help="Activer l'enregistrement dans un fichier de log");
-args=parser.parse_args();
+args=parser.parse_args(); 
 
-print(args);
+print("Taille du fichier: : ",fileinfo.file_size(args.file))
+print("Date de derniere modification: : ",fileinfo.file_modification(args.file))
+print("Encodage: ",fileinfo.file_encoding(args.file))
