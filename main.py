ad2e534a909e0ea49c927eb0ae64dcd4c63d7733
François Flament <contact@leezio.fr>
2018-10-04 10:11:07 +0200

    Adding args parameters

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Data Analysis !")
+import argparse
+
+parser = argparse.ArgumentParser();
+parser.add_argument("-f","--file", nargs="+", help="Chemin du fichier de données");
+parser.add_argument("-u","--user", action="store_true", help="Activer l'interaction utilisateur");
+parser.add_argument("-l","--log", action="store_true", help="Activer l'enregistrement dans un fichier de log");
+args=parser.parse_args();
+
+print(args);
