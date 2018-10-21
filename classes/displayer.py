3002ef1a5d63a6588ab50e37eb083e4ad1cb47eb
Alice Mondja <liloumond@gmail.com>
2018-10-21 22:46:35 +0200

    Vérification de l'orthographe

--- classes/displayer.py
+++ classes/displayer.py
@@ -2,43 +2,43 @@ import numpy;
 
 # Dataset class
 class Displayer:
-    
+
     MAIN_MENU_EXIT = "q";
-    
+
     SECOND_MENU_RETURN = "r";
     SECOND_MENU_FILE = "f";
     SECOND_MENU_DATA = "d";
-    
+
     FILE_INFORMATION_SIZE = "t";
     FILE_INFORMATION_ENCODING = "e";
     FILE_INFORMATION_DATE = "d";
     FILE_INFORMATION_ALL = "a";
-    
+
     DATA_INFORMATION_QUANTITATIVES = "v";
     DATA_INFORMATION_QUALITATIVES = "l";
-    
+
     # Constructor
     def __init__(self, header, file, dataset):
         self.header = header;
         self.file = file;
         self.dataset = dataset;
-    
+
     # Override print function and add breakline
     def printr(self, value = ""):
         print(value + "\n");
-        
+
     # Display header
     def displayHeader(self):
         self.printr(self.header);
-        
+
     # Display file size
     def displayFileSize(self):
         self.printr("Taille du fichier: " + self.file.getSize());
-        
+
     # Display last file modification date
     def displayLastFileModificationDate(self):
-        self.printr("Date de derniere modification: " + self.file.getLastModificationDate());
-        
+        self.printr("Date de dernière modification: " + self.file.getLastModificationDate());
+
     # Display file encoding
     def displayFileEncoding(self):
         self.printr("Encodage: "+ self.file.getEncoding());
@@ -61,35 +61,35 @@ class Displayer:
 
     # Dislpay number of qualitatices varibales
     def displayNbQualitativesVar(self):
-        self.printr("Nombre variables qualitatives: " + self.dataset.getAnalyzer().getNbQualitativesVar().__str__());
+        self.printr("Nombre des variables qualitatives: " + self.dataset.getAnalyzer().getNbQualitativesVar().__str__());
 
     # Display list of qualitatives variable
     def displayQualitativesVarList(self):
-        self.printr("Liste variables qualitatives: " + self.dataset.getAnalyzer().getQualitativesVarList().__str__());
+        self.printr("Liste des variables qualitatives: " + self.dataset.getAnalyzer().getQualitativesVarList().__str__());
 
     # Display list with modality of qualitatives variables
     def displayModalityValues(self):
-        self.printr("Les modalites des variables qualitatives sont: " + self.dataset.getAnalyzer().getModalityValues().__str__());
+        self.printr("Les modalités des variables qualitatives sont: " + self.dataset.getAnalyzer().getModalityValues().__str__());
 
     # Display a list with effective of qualitatives variables
     def displayEffectiveValues(self):
-        self.printr("Les effectifes des variables qualitatives sont:");
+        self.printr("Les effectifs des variables qualitatives sont:");
         for i in range(0, len(self.dataset.getAnalyzer().getEffectiveValues())):
             print(self.dataset.getAnalyzer().getEffectiveValues()[i]);
-    
+
     # Display a list with frequency of qualitatives variables
     def displayFrequencyValues(self):
-        self.printr("Les frequences des variables qualitatives sont:");
+        self.printr("Les fréquences des variables qualitatives sont:");
         for i in range(0, len(self.dataset.getAnalyzer().getFrequencyValues())):
             print(self.dataset.getAnalyzer().getFrequencyValues()[i]);
 
     # Display number of quantitatives variables
     def displayNbQuantitativesVar(self):
-        self.printr("Nombre variables qantitatives: " + self.dataset.getAnalyzer().getNbQuantitativesVar().__str__());
+        self.printr("Nombre des variables quantitatives: " + self.dataset.getAnalyzer().getNbQuantitativesVar().__str__());
 
     # Display list of quantitatives variables
     def displayQuantitativesVarList(self):
-        self.printr("Liste variables qantitatives: " + self.dataset.getAnalyzer().getQuantitativesVarList().__str__());
+        self.printr("Liste des variables quantitatives: " + self.dataset.getAnalyzer().getQuantitativesVarList().__str__());
 
     # Display list with minimal values of quantitatives variables
     def displayMinValues(self):
@@ -101,7 +101,7 @@ class Displayer:
 
     # Display list with median of quantitatives variables
     def displayMedianValues(self):
-        self.printr("Les valeurs medianes des variables quantitatives sont: " + self.dataset.getAnalyzer().getMedianValues().__str__());
+        self.printr("Les valeurs médianes des variables quantitatives sont: " + self.dataset.getAnalyzer().getMedianValues().__str__());
 
     # Display list with average of quantitatives variables
     def displayAverageValues(self):
@@ -109,34 +109,33 @@ class Displayer:
 
     # Display list with standard deviation of quantitatives variables
     def displayStandardDeviationValues(self):
-        self.printr("Les ecarts types des variables quantitatives sont: " + self.dataset.getAnalyzer().getStandardDeviationValues().__str__());
-       
-    # Display main menu 
+        self.printr("Les écarts-types des variables quantitatives sont: " + self.dataset.getAnalyzer().getStandardDeviationValues().__str__());
+
+    # Display main menu
     def displayMainMenu(self):
         self.printr("Que voulez-vous faire ?");
         print("\u0009" + self.SECOND_MENU_FILE + " - Afficher les informations sur le fichier");
         print("\u0009" + self.SECOND_MENU_DATA + " - Afficher les informations sur les donnees");
         self.printr("\u0009" + self.MAIN_MENU_EXIT + " - Quitter")
-     
-    # Display second menu   
+
+    # Display second menu
     def displaySecondMenu(self):
-        self.printr("Quelle informations voulez-vous afficher ?");
-    
-    # Display file menu    
+        self.printr("Quelles informations voulez-vous afficher ?");
+
+    # Display file menu
     def displayFileMenu(self):
         print("\u0009" + self.FILE_INFORMATION_SIZE + " - Afficher la taille du fichier");
         print("\u0009" + self.FILE_INFORMATION_ENCODING + " - Afficher l'encodage du fichier");
         print("\u0009" + self.FILE_INFORMATION_DATE + " - Afficher la date de derniere modification du fichier");
         print("\u0009" + self.FILE_INFORMATION_ALL + " - Afficher l'ensemble des informations");
         self.printr("\u0009" + self.SECOND_MENU_RETURN + " - Retour au menu principal")
-     
+
     # Display data menu
     def displayDataMenu(self):
         print("\u0009" + self.DATA_INFORMATION_QUANTITATIVES + " - Afficher les informations sur les variables quantitatives");
         print("\u0009" + self.DATA_INFORMATION_QUALITATIVES + " - Afficher les informations sur les variables qualitatives");
         self.printr("\u0009" + self.SECOND_MENU_RETURN + " - Retour au menu principal")
-    
-    # Display invalid command message    
+
+    # Display invalid command message
     def displayInvalidCommand(self):
         self.printr("Commande invalide")
-    
--- classes/log.py
+++ classes/log.py
@@ -2,7 +2,7 @@ import os;
 
 # Class log
 class Log:
-    
+
     # Constructor
     def __init__(self, filepath, file, dataset):
         try:
@@ -12,41 +12,40 @@ class Log:
         self.logFile = open(filepath, "a");
         self.file = file;
         self.dataset = dataset;
-        
+
     def write(self, text):
         self.logFile.write(text + "\n\n");
-        
+
     def save(self):
-        
+
         list = [];
-        
+
         # Log all file informations
         list.append("Taille du fichier: " + self.file.getSize());
         list.append("Encodage: "+ self.file.getEncoding());
-        list.append("Date de derniere modification: " + self.file.getLastModificationDate());
-    
+        list.append("Date de dernière modification: " + self.file.getLastModificationDate());
+
         # Log all data informations
         #list.append(currentDataset.getData().__str__());
         #list.append(currentDataset.getColumnsTypes().__str__());
         #list.append(currentDataset.getNbColumns());
         #list.append(currentDataset.getNbLines());
-    
+
         # Log all qualitatives variables informations
-        list.append("Nombre variables qualitatives: " + self.dataset.getAnalyzer().getNbQualitativesVar().__str__());
-        list.append("Liste variables qualitatives: " + self.dataset.getAnalyzer().getQualitativesVarList().__str__());
-        list.append("Les modalites des variables qualitatives sont: " + self.dataset.getAnalyzer().getModalityValues().__str__());
-        list.append("Les effectifes des variables qualitatives sont: " + self.dataset.getAnalyzer().getEffectiveValues().__str__());
-        list.append("Les frequences des variables qualitatives sont: " + self.dataset.getAnalyzer().getFrequencyValues().__str__());
-    
+        list.append("Nombre des variables qualitatives: " + self.dataset.getAnalyzer().getNbQualitativesVar().__str__());
+        list.append("Liste des variables qualitatives: " + self.dataset.getAnalyzer().getQualitativesVarList().__str__());
+        list.append("Les modalités des variables qualitatives sont: " + self.dataset.getAnalyzer().getModalityValues().__str__());
+        list.append("Les effectifs des variables qualitatives sont: " + self.dataset.getAnalyzer().getEffectiveValues().__str__());
+        list.append("Les fréquences des variables qualitatives sont: " + self.dataset.getAnalyzer().getFrequencyValues().__str__());
+
         # Log all quantitatives variables informations
-        list.append("Nombre variables qantitatives: " + self.dataset.getAnalyzer().getNbQuantitativesVar().__str__());
-        list.append("Liste variables qantitatives: " + self.dataset.getAnalyzer().getQuantitativesVarList().__str__());
+        list.append("Nombre des variables qantitatives: " + self.dataset.getAnalyzer().getNbQuantitativesVar().__str__());
+        list.append("Liste des variables qantitatives: " + self.dataset.getAnalyzer().getQuantitativesVarList().__str__());
         list.append("Les valeurs minimales des variables quantitatives sont: " + self.dataset.getAnalyzer().getMinValues().__str__());
         list.append("Les valeurs maximales des variables quantitatives sont: " + self.dataset.getAnalyzer().getMaxValues().__str__());
-        list.append("Les valeurs medianes des variables quantitatives sont: " + self.dataset.getAnalyzer().getMedianValues().__str__());
+        list.append("Les valeurs médianes des variables quantitatives sont: " + self.dataset.getAnalyzer().getMedianValues().__str__());
         list.append("Les moyennes des variables quantitatives sont: " + self.dataset.getAnalyzer().getAverageValues().__str__());
-        list.append("Les ecarts types des variables quantitatives sont: " +  self.dataset.getAnalyzer().getStandardDeviationValues().__str__());
-        
+        list.append("Les écarts-types des variables quantitatives sont: " +  self.dataset.getAnalyzer().getStandardDeviationValues().__str__());
+
         for i in range(0, len(list)):
             self.write(list[i]);
-            
