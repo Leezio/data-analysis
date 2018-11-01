b30877c44b93650d6e1b7ed7480222286cb985f5
François Flament <contact@leezio.fr>
2018-11-01 20:21:48 +0100

    Adding L10N support for log part

--- classes/log.py
+++ classes/log.py
@@ -21,29 +21,29 @@ class Log:
         list = [];
 
         # Log all file informations
-        list.append("Taille du fichier: " + self.file.getSize());
-        list.append("Encodage: "+ self.file.getEncoding());
-        list.append("Date de dernière modification: " + self.file.getLastModificationDate());
+        list.append(self.l10n.getL10n("FILE_SIZE") + " " + self.file.getSize());
+        list.append(self.l10n.getL10n("FILE_DATE") + " " + self.file.getLastModificationDate());
+        list.append(self.l10n.getL10n("FILE_ENCODING") + " " + self.file.getEncoding());
 
         # Log all data informations
-        list.append("Nombre de colonnes: " + currentDataset.getNbColumns());
-        list.append("Nombre de lignes: " + currentDataset.getNbLines());
-        list.append("Nombre des variables qualitatives: " + self.dataset.getAnalyzer().getNbQualitativesVar().__str__());
-        list.append("Liste des variables qualitatives: " + self.dataset.getAnalyzer().getQualitativesVarList().__str__());
-        list.append("Nombre des variables qantitatives: " + self.dataset.getAnalyzer().getNbQuantitativesVar().__str__());
-        list.append("Liste des variables qantitatives: " + self.dataset.getAnalyzer().getQuantitativesVarList().__str__());
+        list.append(self.l10n.getL10n("DATA_NB_COLUMNS") + " " + self.dataset.getNbColumns());
+        list.append(self.l10n.getL10n("DATA_NB_LINES") + " " + self.dataset.getNbLines());
+        list.append(self.l10n.getL10n("DATA_NB_VAR_QUALI") + " " + self.dataset.getAnalyzer().getNbQualitativesVar().__str__());
+        list.append(self.l10n.getL10n("DATA_LIST_VAR_QUALI") + " " + self.dataset.getAnalyzer().getQualitativesVarList().__str__());
+        list.append(self.l10n.getL10n("DATA_NB_VAR_QUANT") + " " + self.dataset.getAnalyzer().getNbQuantitativesVar().__str__());
+        list.append(self.l10n.getL10n("DATA_LIST_VAR_QUANT") + " " + self.dataset.getAnalyzer().getQuantitativesVarList().__str__());
 
         # Log all qualitatives variables informations
-        list.append("Les modalités des variables qualitatives sont: " + self.dataset.getAnalyzer().getModalityValues().__str__());
-        list.append("Les effectifs des variables qualitatives sont: " + self.dataset.getAnalyzer().getEffectiveValues().__str__());
-        list.append("Les fréquences des variables qualitatives sont: " + self.dataset.getAnalyzer().getFrequencyValues().__str__());
+        list.append(self.l10n.getL10n("DATA_MOD_QUALI") + " " + self.dataset.getAnalyzer().getModalityValues().__str__());
+        list.append(self.l10n.getL10n("DATA_EFF_QUALI") + " " + self.dataset.getAnalyzer().getEffectiveValues().__str__());
+        list.append(self.l10n.getL10n("DATA_FREQ_QUALI") + " " + self.dataset.getAnalyzer().getFrequencyValues().__str__());
 
         # Log all quantitatives variables informations
-        list.append("Les valeurs minimales des variables quantitatives sont: " + self.dataset.getAnalyzer().getMinValues().__str__());
-        list.append("Les valeurs maximales des variables quantitatives sont: " + self.dataset.getAnalyzer().getMaxValues().__str__());
-        list.append("Les valeurs médianes des variables quantitatives sont: " + self.dataset.getAnalyzer().getMedianValues().__str__());
-        list.append("Les moyennes des variables quantitatives sont: " + self.dataset.getAnalyzer().getAverageValues().__str__());
-        list.append("Les écarts-types des variables quantitatives sont: " +  self.dataset.getAnalyzer().getStandardDeviationValues().__str__());
+        list.append(self.l10n.getL10n("DATA_MIN_QUANT") + " " + self.dataset.getAnalyzer().getMinValues().__str__());
+        list.append(self.l10n.getL10n("DATA_MAX_QUANT") + " " + self.dataset.getAnalyzer().getMaxValues().__str__());
+        list.append(self.l10n.getL10n("DATA_MED_QUANT") + " " +  self.dataset.getAnalyzer().getMedianValues().__str__());
+        list.append(self.l10n.getL10n("DATA_AVG_QUANT") + " " +  self.dataset.getAnalyzer().getAverageValues().__str__());
+        list.append(self.l10n.getL10n("DATA_SDV_QUANT") + " " +  self.dataset.getAnalyzer().getStandardDeviationValues().__str__());
 
         for i in range(0, len(list)):
             self.write(list[i]);
