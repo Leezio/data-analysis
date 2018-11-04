a48f7e96a7a1cc121fecf7d0d8ae14d9a7658bb8
François Flament <contact@leezio.fr>
2018-11-04 22:00:08 +0100

    Fix indentation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse;
 
 try:
     
-    version = "v1.5.3";
+    version = "v1.5.4";
 
     # Console args -l -u etc...
     parser = argparse.ArgumentParser();
@@ -152,11 +152,11 @@ try:
             # End if
         
             currentLogFile.save(logFilepath);
+        
+        except TypeError:
+            pass;
             
     #End if
-        
-    except TypeError:
-        pass;
         
     gui.clear();
 
