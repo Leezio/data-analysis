e89f0391b1b1a40ad215706762c79013ca18dbe4
Alice Mondja <liloumond@gmail.com>
2018-11-04 22:43:14 +0100

    Correction Log dans main.py

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from classes import l10n, file, dataset, gui, log;
 import argparse;
 
 try:
-    
+
     version = "v1.5.2";
 
     # Console args -l -u etc...
     parser = argparse.ArgumentParser();
     parser.add_argument("-f","--filepath", nargs="?", help="Chemin du fichier de donnees");
     parser.add_argument("-u","--user", action="store_true", help="Activer l'interaction utilisateur");
-    parser.add_argument("-l","--log", nargs="?", help="Activer l'enregistrement dans un fichier de log");
+    parser.add_argument("-l","--log", action="store_true", help="Activer l'enregistrement dans un fichier de log");
+    parser.add_argument("-p","--logFilePath", nargs="?", help="Chemin du fichier de log");
     parser.add_argument("-v","--version", action="store_true", help="Retourne la version du script");
     args=parser.parse_args();
 
@@ -40,120 +41,121 @@ try:
 
     # If user want options menu
     if args.user:
-    
+
         while (mainAnswer != gui.getDisplayer().MAIN_MENU_EXIT):
-        
+
             # Display main choice menu
             gui.getDisplayer().displayMainMenu();
-        
+
             mainAnswer = gui.newAnswer(currentL10n.getL10n("ANSWER") + " ");
-        
+
             while (mainAnswer != gui.getDisplayer().MAIN_MENU_EXIT and secondAnswer != gui.getDisplayer().SECOND_MENU_RETURN):
-                 
-                # If the users want a menu, display second choice menu 
+
+                # If the users want a menu, display second choice menu
                 if (mainAnswer == gui.getDisplayer().SECOND_MENU_FILE):
                     gui.getDisplayer().displaySecondMenu();
                     gui.getDisplayer().displayFileMenu();
-                
+
                 elif (mainAnswer == gui.getDisplayer().SECOND_MENU_DATA):
                     gui.getDisplayer().displaySecondMenu();
                     gui.getDisplayer().displayDataMenu();
-        
+
                 elif (mainAnswer == gui.getDisplayer().MAIN_MENU_EXIT):
                     break;
-            
+
                 else:
                     gui.getDisplayer().displayInvalidCommand();
                     break;
-            
+
                 # End if
-            
+
                 secondAnswer = gui.newAnswer(currentL10n.getL10n("ANSWER") + " ");
-                
+
                 if (secondAnswer == gui.getDisplayer().FILE_INFORMATION_SIZE):
                     gui.getDisplayer().displayFileSize();
-                
+
                 elif (secondAnswer == gui.getDisplayer().FILE_INFORMATION_ENCODING):
                     gui.getDisplayer().displayFileEncoding();
-                
+
                 elif (secondAnswer == gui.getDisplayer().FILE_INFORMATION_DATE):
                     gui.getDisplayer().displayLastFileModificationDate();
-                
+
                 elif (secondAnswer == gui.getDisplayer().FILE_INFORMATION_ALL):
                     # Display all file informations
                     gui.getDisplayer().displayAllFileInformations();
-                
+
                 elif (secondAnswer == gui.getDisplayer().DATA_INFORMATION_TABLE):
                     # Display all data informations
                     gui.getDisplayer().displayAllDataInformations();
-                
+
                 elif (secondAnswer == gui.getDisplayer().DATA_INFORMATION_QUANTITATIVES):
                     # Display quantitatives variables informations
                     gui.getDisplayer().displayAllQualitativesVariablesInformations();
-                
+
                 elif (secondAnswer == gui.getDisplayer().DATA_INFORMATION_QUALITATIVES):
                     # Display qualitatives variables informations
                     gui.getDisplayer().displayAllQuantitativesVariablesInformations();
-                
+
                 elif (secondAnswer == gui.getDisplayer().DATA_INFORMATION_PLOT):
                     # Create plot box
                     gui.getDisplayer().displayCreatePlotSuccess();
                     currentDataset.createPlotBox();
-                
+
                 elif (secondAnswer == gui.getDisplayer().SECOND_MENU_RETURN):
                     secondAnswer = False;
                     break;
-            
+
                 else:
                     gui.getDisplayer().displayInvalidCommand();
                     break;
-            
+
                 # End if
-            
-                gui.pressAnyKey(); 
-        
+
+                gui.pressAnyKey();
+
             # End while
-        
-        #End while      
+
+        #End while
     else:
-    
+
         gui.getDisplayer().displayAll();
-    
+
         # Create plot box
         gui.getDisplayer().displayCreatePlotSuccess();
         currentDataset.createPlotBox();
-    
+
         # End if
 
         gui.pressAnyKey();
-    
+
     # End if
-    
+    if args.log:
+        
     # Enable log file
     currentLogFile = log.Log(currentFile, currentDataset, currentL10n);
-        
+
     try:
         if args.log is not None:
             logFilepath = args.log;
-            
+
             # If log filepath end already with '/' character
             if args.log.endswith("/") is False:
                 logFilepath += "/" + currentLogFile.DEFAULT_LOG_FILE_NAME;
             else:
                 logFilepath += currentLogFile.DEFAULT_LOG_FILE_NAME;
-               
+
             # End if
-        
+
         else:
             logFilepath = currentLogFile.DEFAULT_LOG_FILE_NAME;
-        
+
         # End if
-        
+
         currentLogFile.save(logFilepath);
-        
+
     except TypeError:
         pass;
-        
+
     gui.clear();
 
     # Exit message
